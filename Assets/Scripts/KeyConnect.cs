82589e989fecb4b7735bd1fe058211bee9dbcf06
Morgan <mocsmith@ucsc.edu>
2019-03-13 13:24:21 -0700

    Assignment 8 Update

---
+++ Assets/Scripts/KeyConnect.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyConnect : MonoBehaviour
+{
+    public int KeyNumber;
+    private Vector3 origPos;
+    private Vector3 screenPoint;
+    private Vector3 offset;
+    bool hasJoint;
+    public int jointCount = 0;
+
+    void Start()
+    {
+        origPos = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            FixedJoint fixedJoint = GetComponent<FixedJoint>();
+            Destroy(fixedJoint);
+            StartCoroutine(BreakJoints());
+        }
+    }
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(transform.position);
+        offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = curPosition;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Rigidbody>() != null && !hasJoint && collision.gameObject.tag == "KeyFrag")
+        {
+            gameObject.AddComponent<FixedJoint>();
+            gameObject.GetComponent<FixedJoint>().connectedBody = collision.rigidbody;
+            hasJoint = true;
+            jointCount = jointCount + 1;
+        }
+    }
+
+    IEnumerator BreakJoints()
+    {
+        yield return new WaitForSeconds(0.05f);
+        transform.position = origPos;
+        hasJoint = false;
+        jointCount = 0;
+        StopCoroutine(BreakJoints());
+    }
+}
---
+++ Assets/Scripts/UIScript.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIScript : MonoBehaviour
+{
+    GameObject keyText;
+    GameObject incText;
+
+    void Awake()
+    {
+        keyText = GameObject.Find("CheckKey");
+        incText = GameObject.Find("IncorrectText");
+    }
+
+    void FixedUpdate()
+    {
+        if (GameObject.Find("KeyFrag").GetComponent<KeyConnect>().jointCount +
+            GameObject.Find("KeyFrag1").GetComponent<KeyConnect>().jointCount +
+            GameObject.Find("KeyFrag2").GetComponent<KeyConnect>().jointCount == 3)
+        {
+            keyText.SetActive(true);
+
+            if (Input.GetKeyDown(KeyCode.T))
+            {
+                StartCoroutine(IncorrectText());
+            }
+        }
+        else
+        {
+            keyText.SetActive(false);
+            incText.SetActive(false);
+        }
+    }
+
+    IEnumerator IncorrectText()
+    {
+        incText.SetActive(true);
+        yield return new WaitForSeconds(1f);
+        incText.SetActive(false);
+        StopCoroutine(IncorrectText());
+    }
+}
